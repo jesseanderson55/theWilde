50f0c2294bb030245e5e8dadafd359d92a3d9cb6
Jesse Anderson <jand518@wgu.edu>
2021-01-25 16:48:24 -0500

    Update0.04
    Creating a new command today, but first removing some lines from ready event that is buggy and needs rework. Also removing the social command. Wont be using it for this bot.

--- thewilde/src/events/ready/ReadyEvent.js
+++ thewilde/src/events/ready/ReadyEvent.js
@@ -16,8 +16,8 @@ module.exports = class ReadyEvent extends BaseEvent {
       status: 'online' 
     })
       .catch(console.error);
-      client.user.setUsername(`${client.prefix} ${client.user.username}`)
-      .then(user => console.log(`My new username is ${user.username}`))
-      .catch(console.error);
+      //client.user.setUsername(`${client.prefix} ${client.user.username}`)
+      //.then(user => console.log(`My new username is ${user.username}`))
+      //.catch(console.error);
   }
 }
